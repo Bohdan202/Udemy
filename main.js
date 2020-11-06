df15475851731b611329c4fdbb467cf6f4121507
Bohdan <ni27bo@icloud.com>
2020-11-06 18:05:35 +0200

    first exercise

---
+++ main.js
@@ -0,0 +1,25 @@
+/* Первое задание  */
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?");
+
+/* Второе задание */
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+/* Третье задание */
+
+let a = prompt("Один из последних просмотреных фильмов?"),
+    b = +prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотреных фильмов?"),
+    d = +prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
